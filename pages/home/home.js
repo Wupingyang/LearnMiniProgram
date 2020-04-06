b75d4fd899d5c2a72e4193a5ff748424aa0b864e
Wupingyang <752968143@qq.com>
2020-04-07 00:10:47 +0800

    小程序初体验

--- pages/home/home.js
+++ pages/home/home.js
@@ -1,66 +1,28 @@
-// pages/home/home.js
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-
+    name: 'WittyWu',
+    age: 18,
+    students:[
+      {id: 110, name: 'kobe', age: 15},
+      {id: 111, name: 'jame', age: 16},
+      {id: 112, name: 'curry', age: 17},
+      {id: 113, name: 'WittyWu', age: 18}
+    ],
+    cnt: 0
   },
+  handleBtnClickAdd(){
+    // 1. 错误做法：界面不会刷新
+    // this.data.cnt += 1
+    // console.log(this.data.cnt)
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
+    // 2. this.setData()
+    this.setData({
+      cnt: this.data.cnt + 1
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  handleBtnClickSub(){
+    this.setData({
+      cnt: this.data.cnt - 1
+    })
   }
 })
