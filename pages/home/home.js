1d0c7fe5eaa172227d0bb7cb1e049b99f936e908
Wupingyang <752968143@qq.com>
2020-04-19 21:17:16 +0800

    事件解析

--- pages/home/home.js
+++ pages/home/home.js
@@ -1,66 +1,61 @@
 // pages/home/home.js
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-
+    titles: ['衣服', '裤子', '鞋子']
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
+  handleBtnClick() {
+    console.log('按钮发生点击')
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
+  handleTouchStart() {
+    console.log('touchStart')
   },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
+  handleTouchMove() {
+    console.log('touchMove')
   },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
+  handleTouchEnd() {
+    console.log('touchEnd')
   },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
+  handleTap() {
+    console.log('tap')
   },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
+  handleLongPress() {
+    console.log('longPress')
   },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
+  handleEventClick(event) {
+    console.log('------', event)
   },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  handleEventEnd(event) {
+    console.log('++++++', event)
+  },
+  handleInter(event) {
+    console.log('内层', event)
+  },
+  handleOuter(event) {
+    console.log('外层', event)
+  },
+  handleItemClick(event) {
+    console.log(event);
+    const dataset = event.currentTarget.dataset;
+    const index = dataset.index;
+    const title = dataset.item;
+    console.log(title, index)
+  },
+  handleCaptureView1() {
+    console.log('handleCaptureView1')
+  },
+  handleCaptureView2() {
+    console.log('handleCaptureView2')
+  },
+  handleCaptureView3() {
+    console.log('handleCaptureView3')
+  },
+  handleBindView1() {
+    console.log('handleBindView1')
+  },
+  handleBindView2() {
+    console.log('handleBindView2')
+  },
+  handleBindView3() {
+    console.log('handleBindView3')
   }
 })
