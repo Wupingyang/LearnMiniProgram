42611a699777e79b2c583fa3283485df187d566c
Wupingyang <752968143@qq.com>
2020-05-04 22:00:05 +0800

    小程序弹窗和分享

--- pages/home/home.js
+++ pages/home/home.js
@@ -1,66 +1,63 @@
-// pages/home/home.js
 Page({
-
-  /**
-   * 页面的初始数据
-   */
-  data: {
-
+  handleShowToast() {
+    wx.showToast({
+      title: '消息提示框',
+      icon: 'success',
+      duration: 2000,
+      mask: true,
+      //image: '/assets/icon/icon.jpg'
+      success() {
+        console.log('展示弹窗成功')
+      },
+      fail() {
+        console.log('展示弹窗失败')
+      },
+      complete() {
+        console.log('展示弹窗完成')
+      }
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
+  handleShowModal() {
+    wx.showModal({
+      title: '提示',
+      content: '模态对话框',
+      //showCancel: false,
+      cancelText: '返回',
+      cancelColor: 'red',
+      success(res) {
+        console.log(res)
+        if (res.cancel){
+          console.log('用户点击取消')
+        }else if (res.confirm) {
+          console.log('用户点击确定')
+        }
+      }
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
+  handleShowLoading() {
+    wx.showLoading({
+      title: 'Loading',
+      mask: true
+    })
+    setTimeout(()=>{
+      wx.hideLoading()
+    }, 1000)
   },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
+  handleShowActionSheet() {
+    wx.showActionSheet({
+      itemList: ['相册', '拍照'],
+      itemColor: 'red',
+      success(res) {
+        console.log(res)
+        console.log(res.tapIndex)
+      }
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  onShareAppMessage() {
+    return{
+      title: '转发标题',
+      path: '/pages/about/about',
+      imageUrl: '/assets/hacker.jpg'
+    }
   }
 })
